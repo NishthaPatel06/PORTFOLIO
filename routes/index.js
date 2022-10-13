754cfccd9c5634a712e3cd4076a92219ef84de6f
NishthaPatel06 <200474598@student.georgianc.on.ca>
2022-10-13 18:54:57 -0400

    About Page done!

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nishtha Patel' });
+  res.render('index', { title: 'My portfolio' });
 });
 
 /*GET About Page*/
 router.get('/about', (req, res) => {
   res.render('about',{
   title: 'About this Site',
-  content: 'We will put stuff here'
+  content: 'About Me'
+  })
+})
+
+/* Skills Page*/
+router.get('/Skills', (req, res) => {
+  res.render('skills',{
+  title: 'My skills',
+  content: 'Here it is'
+  })
+})
+
+
+
+
+
+/*GET Projects Page*/
+router.get('/Projects', (req, res) => {
+  res.render('Projects',{
+  title: 'My Projects',
+  content: 'Here it is'
+  })
+})
+
+/*GET Contact Page*/
+router.get('/contact', (req, res) => {
+  res.render('Contact',{
+  title: 'Contact',
+  content: 'Here it is'
   })
 })
 
